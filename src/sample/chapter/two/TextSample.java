461a1e0ce66ae5d079152ac6f99d6ce14950cf37
namannigam <namannigam12@gmail.com>
2018-02-06 08:40:13 +0530

    Tue Feb  6 08:40:13 IST 2018

--- src/sample/chapter/two/TextSample.java
+++ src/sample/chapter/two/TextSample.java
@@ -3,10 +3,11 @@ package sample.chapter.two;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
 
 public class TextSample extends Application {
@@ -16,8 +17,23 @@ public class TextSample extends Application {
         Text txt = new Text("Hello, JavaFX!");
         txt.setFont(Font.font("Courier New", FontWeight.BOLD, FontPosture.ITALIC, 20));
 
+        // gradient fill
+        Stop[] stops =
+                new Stop[]{new Stop(0, Color.BLACK), new Stop(1, Color.DARKGRAY), new Stop(0.5, Color.ANTIQUEWHITE)};
+        LinearGradient gradient = new LinearGradient(50, 50, 250, 50, false, CycleMethod.NO_CYCLE, stops);
+        txt.setFill(gradient);
+
+
+        Text txt1 = new Text("Text1");
+        txt1.setFont(Font.font ("Courier New", 15));
+        Text txt2 = new Text("Text2");
+        txt2.setFont(Font.font ("Times New Roman", 20));
+        Text txt3 = new Text("Text3");
+        txt3.setFont(Font.font ("Arial", 30));
+        TextFlow textFlow = new TextFlow(txt1, txt2, txt3);
+
         StackPane root = new StackPane();
-        root.getChildren().addAll(txt);
+        root.getChildren().addAll(txt, textFlow);
 
         Scene scene = new Scene(root, 200, 150);
         primaryStage.setScene(scene);
